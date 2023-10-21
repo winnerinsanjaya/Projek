35560cd8d9a7fb7bc84a0f31dd418f35edad77ce
otoypsd <gagantuar12@gmail.com>
2023-10-21 19:15:45 +0700

    terbaru

--- latihan/Assets/Script/PauseMenu.cs
+++ latihan/Assets/Script/PauseMenu.cs
@@ -5,28 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject PausePanel;
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
     
     public void Pause()
     {
-        PausePanel.SetActive(true);
-        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     public void Continue()
     {
-        PausePanel.SetActive(false); 
-        Time.timeScale = 1;
+        pauseMenu.SetActive(false); 
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 
-    public void BackTomainmenu()
+    public void BackToMainMenu()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadScene(0);
     }
 }
